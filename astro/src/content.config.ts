8ff106cd1097a09d08935df5203d7417f62329fb
Tito Silva <jt.mat@hotmail.com>
2026-02-15 16:00:50 -0300

    feat(wip): working on posts page

---
+++ astro/src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, reference } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/posts" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    authors: z.array(reference("people")),
+  })
+});
+
+const people = defineCollection({
+  loader: glob({ pattern: "**/*.json", base: "./src/data/people" }),
+  schema: z.object({
+    name: z.string(),
+  })
+});
+
+export const collections = {
+  posts,
+  people,
+};
